3c046169e2f4cb0902b7c3d7896bf2f0c228a113
Denis Holc <holc.denis7@gmail.com>
2023-12-11 13:13:23 +0100

    sixth-section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import SixthSection from "./Components/Sixth-Section";
+import SeventhSection from "./Components/Seventh-Section";
 
 function App() {
   return (
     <>
       <SixthSection />
+      <SeventhSection />
     </>
   );
 }
---
+++ src/Components/Seventh-Section/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./SeventhSection.css";
+import seventhSectionBackground from "../../assets/Sixth-Seventh-Section/Seventh-Section-Background.svg";
+
+const SeventhSection = () => {
+  return <div className=" dog-human"></div>;
+};
+
+export default SeventhSection;
